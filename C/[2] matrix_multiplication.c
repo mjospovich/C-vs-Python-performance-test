1f803690e284ef7c1094b49029bcd8fc2667e1a9
Martin <93053179+mjospovich@users.noreply.github.com>
2024-04-27 13:20:00 +0200

    updated test func 2 still not complete

--- C/[2] matrix_multiplication.c
+++ C/[2] matrix_multiplication.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+//#include "time_mem_measure.c"
 
 #define MATRIX_SIZE 1000
+#define MAX_CHAR 5000
 
 void matrix_multiplication() {
-    printf("gotta go\n");
     FILE *fp1, *fp2;
-    int matrix1[MATRIX_SIZE][MATRIX_SIZE], matrix2[MATRIX_SIZE][MATRIX_SIZE], result[MATRIX_SIZE][MATRIX_SIZE];
     int i, j, k;
     char ch;
+    char row[MAX_CHAR];
+
+    int **matrix1 = (int**)malloc(sizeof(int) * MATRIX_SIZE * MATRIX_SIZE);
+    int **matrix2 = malloc(sizeof(int) * MATRIX_SIZE * MATRIX_SIZE);
+    int **result = malloc(sizeof(int) * MATRIX_SIZE * MATRIX_SIZE);
 
     // Open matrix1 file
     fp1 = fopen("../assets/matrix1.csv", "r");
@@ -24,31 +30,51 @@ void matrix_multiplication() {
         fclose(fp1);
         return;
     }
-
-    // print value of fp1 and fp2
-    //printf("fp1: %c\n", fgetc(fp1));
-    
-
+    printf("Files opened.\n");
     // Read matrix1 from file
     for (i = 0; i < MATRIX_SIZE; i++) {
         j = 0;
-        while ((ch = fgetc(fp1)) != '\n' && ch != EOF) {
-            // ungetc(ch, fp1);
-            printf("ch: %c\n", ch);
-            //fscanf(fp1, "%d,", &matrix1[i][j++]);
+        
+        if (feof(fp1) != 1)
+        {
+            fgets(row, MAX_CHAR, fp1);
+
+            // split row by comma
+            char *token = strtok(row, ",");
+            while (token != NULL)
+            {
+                matrix1[0][0] = 0;
+                token = strtok(NULL, ",");
+                j++;
+            }
         }
+        strcpy(row, "");
     }
+    printf("Matrix 1 read.\n");
 
-    // // Read matrix2 from file
-    // for (i = 0; i < MATRIX_SIZE; i++) {
-    //     j = 0;
-    //     while ((ch = fgetc(fp2)) != '\n' && ch != EOF) {
-    //         ungetc(ch, fp2);
-    //         fscanf(fp2, "%d,", &matrix2[i][j++]);
-    //     }
-    // }
+    // Read matrix2 from file
+    for (i = 0; i < MATRIX_SIZE; i++) {
+        j = 0;
+        
+        if (feof(fp2) != 1)
+        {
+            fgets(row, MAX_CHAR, fp2);
 
-    // // Perform matrix multiplication
+            // split row by comma
+            char *token = strtok(row, ",");
+            while (token != NULL)
+            {
+                matrix2[i][j] = atoi(token);
+                token = strtok(NULL, ",");
+                j++;
+            }
+        }
+        strcpy(row, "");
+    }
+    printf("Matrix 2 read.\n");
+    
+
+    // Perform matrix multiplication
     // for (i = 0; i < MATRIX_SIZE; i++) {
     //     for (j = 0; j < MATRIX_SIZE; j++) {
     //         result[i][j] = 0;
@@ -61,9 +87,11 @@ void matrix_multiplication() {
     // Close the files
     fclose(fp1);
     fclose(fp2);
+    printf("Matrix multiplication done.\n");
 }
 
 int main() {
     matrix_multiplication();
+    //print_time_and_memory(matrix_multiplication, 1);
     return 0;
 }
